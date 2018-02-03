6e4a1443e7df1dcffcbcd2640ba06600b525d77b
eloylp <eloy@sandboxwebs.com>
2018-02-04 00:42:21 +0100

    Adding more movements and tests.

---
+++ test/index.test.js
@@ -0,0 +1,13 @@
+const test = require('ava')
+const PakistranoCameraControl = require('../lib/index')
+const pakistranoCameraControl = new PakistranoCameraControl({
+  host: '10.0.8.4',
+  user: 'admin',
+  password: 'admin'
+})
+
+test('Ensure exception on bad camera request.', (t) => {
+  const cameraResponse = '<<<  >>'
+  const error = t.throws(() => pakistranoCameraControl._checkCameraResponse(cameraResponse))
+  t.is(error.message, 'Camera response is not correct: ' + cameraResponse)
+})
--- test/integration.test.js
+++ test/integration.test.js
@@ -8,8 +8,20 @@ const pakistranoCameraControl = new PakistranoCameraControl({
   password: 'admin'
 })
 
+test.afterEach(async () => {
+  await pakistranoCameraControl.stopAxes()
+})
+
 test.serial('Move to right.', async (t) => {
   await t.notThrows(pakistranoCameraControl.startMoveToRight())
+})
+
+test.serial('Move to left.', async (t) => {
+  await t.notThrows(pakistranoCameraControl.startMoveToLeft())
+})
+
+test.serial('Camera center.', async (t) => {
+  await t.notThrows(pakistranoCameraControl.centerCamera())
 })
 
 test.serial('Stopping axes.', async (t) => {
