7cab063382d4b11da527ba9be2b003fd12751ddb
eloylp <eloy@sandboxwebs.com>
2018-02-04 13:53:27 +0100

    Refactoring public interface. All movements added.

--- test/interface.test.js
+++ test/interface.test.js
@@ -12,18 +12,44 @@ test.afterEach(async () => {
   await pakistranoCameraControl.stopAxes()
 })
 
+/// Movements
 test.serial('Move to right integration test.', async (t) => {
-  await t.notThrows(pakistranoCameraControl.startMoveToRight())
+  await t.notThrows(pakistranoCameraControl.startMoveRight())
 })
 
 test.serial('Move to left integration test.', async (t) => {
-  await t.notThrows(pakistranoCameraControl.startMoveToLeft())
+  await t.notThrows(pakistranoCameraControl.startMoveLeft())
 })
 
-test.serial('Camera center integration test.', async (t) => {
-  await t.notThrows(pakistranoCameraControl.centerCamera())
+test.serial('Move to up integration test.', async (t) => {
+  await t.notThrows(pakistranoCameraControl.startMoveUp())
+})
+
+test.serial('Move to down integration test.', async (t) => {
+  await t.notThrows(pakistranoCameraControl.startMoveDown())
+})
+
+test.serial('Move to bottom right integration test.', async (t) => {
+  await t.notThrows(pakistranoCameraControl.startMoveBottomRight())
+})
+
+test.serial('Move to bottom left integration test.', async (t) => {
+  await t.notThrows(pakistranoCameraControl.startMoveBottomLeft())
+})
+
+test.serial('Move to upper left integration test.', async (t) => {
+  await t.notThrows(pakistranoCameraControl.startMoveUpLeft())
+})
+
+test.serial('Move to upper right integration test.', async (t) => {
+  await t.notThrows(pakistranoCameraControl.startMoveUpRight())
 })
 
 test.serial('Stopping axes integration test.', async (t) => {
   await t.notThrows(pakistranoCameraControl.stopAxes())
+})
+
+/// Behaviours
+test.serial('Camera center integration test.', async (t) => {
+  await t.notThrows(pakistranoCameraControl.centerCamera())
 })
