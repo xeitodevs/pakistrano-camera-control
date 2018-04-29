43bebb5348bbd026f482d1e515ee997b0708376c
eloylp <e.lopez@xeitodevs.com>
2018-04-29 19:40:32 +0200

    Updated snapshot, now returns proper buffer.

--- test/snapshot.test.js
+++ test/snapshot.test.js
@@ -3,5 +3,5 @@ const pakistranoCameraControl = require('../lib/pakistranoFactory')
 
 test('Snapshot integration test.', async (t) => {
   const snapshot = await pakistranoCameraControl.getSnapshot()
-  t.true(snapshot.length > 20000)
+  t.true(snapshot.byteLength > 10000)
 })
